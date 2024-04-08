e796b7192af0c3b8e1e4dbd37949d932515ef308
hancc9191 <105869123+hancc9191@users.noreply.github.com>
2024-04-08 20:52:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import requests, json, re, os
+import requests
+import json
+import os
 
 session = requests.session()
-# 配置用户名（一般是邮箱）
-email = os.environ.get('EMAIL')
-# 配置用户名对应的密码 和上面的email对应上
-passwd = os.environ.get('PASSWD')
+
+# 配置用户名和密码（一般是邮箱）
+email1 = os.environ.get('EMAIL1')
+passwd1 = os.environ.get('PASSWD1')
+
+email2 = os.environ.get('EMAIL')
+passwd2 = os.environ.get('PASSWD')
+
 # server酱
 SCKEY = os.environ.get('SCKEY')
 # PUSHPLUS
 Token = os.environ.get('TOKEN')
+
 def push(content):
     if SCKEY != '1':
         url = "https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, 'ikuuu签到', content)
@@ -22,35 +29,49 @@ def push(content):
     else:
         print('未使用消息推送推送！')
 
-# 会不定时更新域名，记得Sync fork
+def login_and_checkin(email, passwd):
+    try:
+        print(f'进行账号 {email} 的登录...')
+        login_url = 'https://ikuuu.pw/auth/login'
+        check_url = 'https://ikuuu.pw/user/checkin'
+        info_url = 'https://ikuuu.pw/user/profile'
 
-login_url = 'https://ikuuu.pw/auth/login'
-check_url = 'https://ikuuu.pw/user/checkin'
-info_url = 'https://ikuuu.pw/user/profile'
+        header = {
+            'origin': 'https://ikuuu.me',
+            'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+        }
+        data = {
+            'email': email,
+            'passwd': passwd
+        }
 
-header = {
-        'origin': 'https://ikuuu.me',
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-}
-data = {
-        'email': email,
-        'passwd': passwd
-}
+        # 登录
+        response = session.post(url=login_url, headers=header, data=data).json()
+        print(response['msg'])
+
+        # 获取账号名称
+        info_html = session.get(url=info_url, headers=header).text
+        info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
+        print(info)
+
+        # 进行签到
+        result = session.post(url=check_url, headers=header).json()
+        print(result['msg'])
+        content = result['msg']
+
+        return content
+
+    except Exception as e:
+        print(f'账号 {email} 签到失败')
+        print(str(e))
+        raise
+
 try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
-    # 获取账号名称
-    info_html = session.get(url=info_url,headers=header).text
-#     info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
-#     print(info)
-    # 进行签到
-    result = json.loads(session.post(url=check_url,headers=header).text)
-    print(result['msg'])
-    content = result['msg']
-    # 进行推送
-    push(content)
-except:
+    contents = []
+    contents.append(login_and_checkin(email, passwd))
+    contents.append(login_and_checkin(email1, passwd1))
+    push("\n".join(contents))
+except Exception as e:
     content = '签到失败'
     print(content)
     push(content)
